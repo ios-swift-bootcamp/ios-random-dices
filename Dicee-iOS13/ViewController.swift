1d8958eb0507cbc43f44cc65294a7e74fb067bae
Porfirio Chávez <hola@porfi.dev>
2022-06-10 11:33:07 -0500

    Add solution code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // @IBOutlet allows interact with view Elements
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    var dices: [UIImage] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Change Image
+        // Using literals #imageLiteral(
+        // leftDiceImageView.image = #imageLiteral(resourceName: "DiceSix")
+        
+        // Named
+        leftDiceImageView.image = UIImage(named: "DiceOne")
+        leftDiceImageView.alpha = 5.0
+        
+        rightDiceImageView.image = UIImage(imageLiteralResourceName: "DiceSix")
+        
+        dices = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ]
     }
 
-
+    // IBAction let me send interactions from user
+    @IBAction func onPressRollButton(_ sender: UIButton) {
+        print("button clicked")
+        leftDiceImageView.image = dices[getRandomIndex()]
+        rightDiceImageView.image = dices.randomElement()
+    }
+    
+    func getRandomIndex() -> Int {
+        return Int.random(in: 0...dices.count-1)
+    }
 }
 
